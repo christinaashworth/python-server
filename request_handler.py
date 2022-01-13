b1b6c1c317e6e34118f08cf9b0c2cd913ad74727
christinaashworth <christina.ashworth@ymail.com>
2022-01-13 14:06:47 -0600

    delete and edit

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, create_animal, get_all_locations, get_single_location, create_location, get_all_customers, get_single_customer, create_customer, get_all_employees, get_single_employee, create_employee
+from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_all_locations, get_single_location, create_location, delete_location, update_location, get_all_customers, get_single_customer, create_customer, delete_customer, update_customer,get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
 import json
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -121,9 +121,46 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any PUT request.
 
     def do_PUT(self):
-        """Handles PUT requests to the server
-        """
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+        if resource == "customers":
+            update_customer(id, post_body)
+        if resource == "employees":
+            update_employee(id, post_body)
+        if resource == "locations":
+            update_location(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+        
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+        if resource == "customers":
+            delete_customer(id)
+        if resource == "employees":
+            delete_employee(id)
+        if resource == "locations":
+            delete_location(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def parse_url(self, path):
         # Just like splitting a string in JavaScript. If the
@@ -145,6 +182,7 @@ class HandleRequests(BaseHTTPRequestHandler):
             pass  # Request had trailing slash: /animals/
 
         return (resource, id)  # This is a tuple
+    
 
 # This function is not inside the class. It is the starting
 # point of this application.
